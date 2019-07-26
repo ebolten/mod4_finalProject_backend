a2cde36547bdb8a84ef0b65e505a69bedbb6308d
Emily Bolten <emilybolten@gmail.com>
2019-07-26 09:33:25 -0400

    fetch call to Game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,24 @@ class GamesController < ApplicationController
         render json: Game.all
     end
 
+    def show
+        render json: Game.find(params['id'])
+    end
+
+    def new
+        @game = Game.new
+    end
+
+    def create
+        @game = Game.create(game_params)
+        render json: @game
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:user_id,:restaurant_id,:score,:level,:money)
+    end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
     def index
         render json: User.all
     end
+
+    def show
+        render json: User.find(params['id'])
+    end
     
 end
