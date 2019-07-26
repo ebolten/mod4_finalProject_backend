bf79fe58b13fddc739e8dee8d118afcddfd735d3
keithburgie <krburgie@gmail.com>
2019-07-26 09:05:39 -0400

    New restaurants in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)/
 
+User.destroy_all
+Restaurant.destroy_all
+Game.destroy_all
+
 emily = User.create( username:'cake_gurl',password_digest:'password123',avatar:'image.url' )
 keith = User.create( username:'keith',password_digest:'password123',avatar:'image.url' )
 
-restaurant_one = Restaurant.create( food:'pancake' )
+restaurant_one = Restaurant.create( food:'pancakes' )
+restaurant_two = Restaurant.create( food:'eggs' )
+restaurant_three = Restaurant.create( food:'burgers' )
 
 game_one = Game.create( user_id:1,restaurant_id:1,score:500,level:8,money:2758247 )
 game_two = Game.create( user_id:2,restaurant_id:1,score:5,level:1,money:8 )
